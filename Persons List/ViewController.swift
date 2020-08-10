d39ca9b7901d03ace2bd586efd0a6b74f50170f9
kairzhan8 <kairzhankural@gmail.com>
2020-08-11 00:17:53 +0600

    Initial Commit

---
+++ Persons List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Persons List
+//
+//  Created by Kairzhan Kural on 8/11/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
